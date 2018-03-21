ac85ee938030fd809521a2d14e70d6747bb5607a
Joseph Semlak <semlak@gmail.com>
2018-03-21 01:29:06 -0500

    added npm scripts

--- db/seedWithSequelize.js
+++ db/seedWithSequelize.js
@@ -1,7 +1,9 @@
 'use strict';
 
 /*
-to run: from app root directory (without quotes):
+to run: 'npm run seed'
+
+Or, from app root directory (without quotes):
 'node ./db/seedWithSequelize.js'
 */
 
@@ -20,33 +22,47 @@ var runOnComplete = function() {
 }
 
 var storyToString = function(story) {
-  return  "Story.id: " + story.id + "\n" + story.dataValues.name + "\n" + story.dataValues.Turns.map(turn=> turn.body).join("\n") ;
+  return  "Story.id: " + story.id + "\n" +
+    "Story.name: " + story.dataValues.name + "\n" +
+    "Players: " + story.dataValues.Players.map(player=> player.dataValues.name).join(", ") + "\n" +
+    story.dataValues.Turns.map(turn=> turn.body + " (" + createdPlayers.find(player=> player.id === turn.PlayerId).name +")").join("\n") ;
 }
 
+// the resulting stories and players will be assigned to these references for easy access.
 var createdStories = null;
 var createdPlayers = null;
-// }
-// body, illustration, StoryId, PlayerId, createdAt, updatedAt
+
+
 var seedTurns = function(callback) {
-  // console.log(createdStories)
-  // console.log(createdPlayers);
+  // the players created in seedPlayers() are stored in the global createdPlayers array for convenience. Similarly for stories.
+
+  // creating the following variables is totally unnessary and just done to show which players and stories are assigned to each turn
+  var [pete, andy, allison, betty, ruth, erika, xena, joe] = createdPlayers;
+  var [gangsterStory, swindlerStory, catStory, wizardStory] = createdStories;
+
   Turn.bulkCreate([
-    { body: "In a galaxy far, far away.", sequence: 1, StoryId: createdStories[0].id, PlayerId: createdPlayers[0].id },
-    { body: "In a hole in the ground there lived a hobbit.", sequence: 2, StoryId: createdStories[0].id, PlayerId: createdPlayers[1].id },
-    { body: "And they lived happily ever after.", sequence: 3, StoryId: createdStories[0].id, PlayerId: createdPlayers[2].id },
-    { body: "Terror made me cruel", sequence: 1, StoryId: createdStories[1].id, PlayerId: createdPlayers[0].id },
-    { body: "Some men get the world, some men get ex-hookers and a trip to Arizona.", sequence: 1, StoryId: createdStories[1].id, PlayerId: createdPlayers[2].id },
-    { body: "The only people for me are the mad ones.", sequence: 2, StoryId: createdStories[1].id, PlayerId: createdPlayers[3].id },
-    { body: "It was a bright cold day in April, and the clocks were striking thirteen.", sequence: 1, StoryId: createdStories[2].id, PlayerId: createdPlayers[3].id },
-    { body: "We were the people who were not in the papers. ", sequence: 1, StoryId: createdStories[2].id, PlayerId: createdPlayers[1].id },
-    { body: "It sounds plausible enough tonight, but wait until tomorrow.", sequence: 1, StoryId: createdStories[2].id, PlayerId: createdPlayers[0].id },
-    { body: "Harry was not like every other boy. Harry was a wizard.", sequence: 1, StoryId: createdStories[3].id, PlayerId: createdPlayers[0].id},
-    { body: "Then he did something that was both very brave and very stupid. Every book.", sequence :2, StoryId: createdStories[3].id, PlayerId: createdPlayers[1].id},
-    { body: "Fortunately, he was friends with Hermione, who solved all his problems. And Snape Killed Dumbledore.", sequence: 3, StoryId: createdStories[3].id, PlayerId: createdPlayers[2].id},
-    { body: "Eventually he killed the dark wizard Lord VoldyThing. And Snape loved Lily. The End.", sequence: 4, StoryId: createdStories[3].id, PlayerId: createdPlayers[0].id}
+    { body: "In a galaxy far, far away.", sequence: 1, StoryId: gangsterStory.id, PlayerId: pete.id },
+    { body: "In a hole in the ground there lived a hobbit.", sequence: 2, StoryId: gangsterStory.id, PlayerId: andy.id },
+    { body: "And they lived happily ever after.", sequence: 3, StoryId: gangsterStory.id, PlayerId: pete.id },
+
+    { body: "Terror made me cruel", sequence: 1, StoryId: swindlerStory.id, PlayerId: allison.id },
+    { body: "Some men get the world, some men get ex-hookers and a trip to Arizona.", sequence: 1, StoryId: swindlerStory.id, PlayerId:betty.id },
+    { body: "The only people for me are the mad ones.", sequence: 2, StoryId: swindlerStory.id, PlayerId: betty.id },
+
+    { body: "It was a bright cold day in April, and the clocks were striking thirteen.", sequence: 1, StoryId: catStory.id, PlayerId: erika.id },
+    { body: "We were the people who were not in the papers. ", sequence: 1, StoryId: catStory.id, PlayerId: ruth.id },
+    { body: "It sounds plausible enough tonight, but wait until tomorrow.", sequence: 1, StoryId: catStory.id, PlayerId: erika.id },
+
+    { body: "Harry was not like every other boy. Harry was a wizard.", sequence: 1, StoryId: wizardStory.id, PlayerId: xena.id},
+    { body: "Then he did something that was both very brave and very stupid. Every book.", sequence :2, StoryId: wizardStory.id, PlayerId: joe.id},
+    { body: "Fortunately, he was friends with Hermione, who solved all his problems. And Snape Killed Dumbledore.", sequence: 3, StoryId: wizardStory.id, PlayerId: joe.id},
+    { body: "Eventually he killed the dark wizard Lord VoldyThing. And Snape loved Lily. The End.", sequence: 4, StoryId: wizardStory.id, PlayerId: xena.id}
   ]).then(function(turns) {
+    // uncomment following line to print all the created turns
     // console.log(turns.map(turn=> turn.dataValues));
-    Story.findAll({include: Turn}).then(function(stories) {
+
+    // this last bit loads all stories, including their turns, and displays.
+    Story.findAll({include: [Turn, Player]}).then(function(stories) {
       console.log(stories.map(story => (storyToString(story))).join("\n\n"));
       runOnComplete();
     })
@@ -57,20 +73,18 @@ var seedTurns = function(callback) {
 var seedPlayers = function(callback) {
   Player.bulkCreate([
     { name: "Pete", email: "Pete@exhausted.com", StoryId: createdStories[0].id },
-    { name: "Andy", email: "Andy@exhausted.com" , StoryId: createdStories[1].id },
-    { name: "Allison", email: "Allison@exhausted.com" , StoryId: createdStories[2].id },
+    { name: "Andy", email: "Andy@exhausted.com" , StoryId: createdStories[0].id },
+    { name: "Allison", email: "Allison@exhausted.com" , StoryId: createdStories[1].id },
     { name: "Betty", email: "Betty@exhausted.com" , StoryId: createdStories[1].id  },
-    { name: "Ruth", email: "Ruth@exhausted.com" , StoryId: createdStories[0].id  },
-    { name: "Xena", email: "warriorprincess@gmail.com" , StoryId: createdStories[2].id  },
-    { name: "Joe", email: "joe@exhausted.com", StoryId: createdStories[1].id }
+    { name: "Ruth", email: "Ruth@exhausted.com" , StoryId: createdStories[2].id  },
+    { name: "Erika", email: "erika@exhausted.com" , StoryId: createdStories[2].id  },
+    { name: "Xena", email: "warriorprincess@gmail.com" , StoryId: createdStories[3].id  },
+    { name: "Joe", email: "joe@exhausted.com", StoryId: createdStories[3].id }
 
   ]).then(function(players) {
     // show results, but just dataValues of the each object
     // console.log("Insertion results: ", players.map(player => player.dataValues));
-    // seedStories();
-    // db.sequelize.close();
     createdPlayers = players.map(player=> player.dataValues);
-    // console.log("createdPlayers: ", createdPlayers)
     seedTurns();
   });
 }
@@ -82,7 +96,7 @@ var seedStories = function(callback) {
   	{ name: "Cats and Assassins"},
     { name: "Harry Potter -- The Very Abridged Edition"}
   ]).then(function(stories) {
-    console.log(stories.map(story => story));
+    // console.log(stories.map(story => story));
     createdStories  = stories.map(story => story.dataValues);
     if (typeof callback === "function" ) {
       // callback(stories);
@@ -97,7 +111,8 @@ var seedStories = function(callback) {
 
 
 db.sequelize.sync({force: true}).then(function() {
-  // seedPlayers will call seedstories when done. seedStories will call seedTurns when done. seedTurns will return joined results when done.
-  // seedPlayers();
+  // seedStories will call run, and then call seedPlayers when done.
+  // seedPlayers will call seedTurns when done.
+  // seedTurns will return joined results when done.
   seedStories();
 });
