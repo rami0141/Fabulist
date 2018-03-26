e3a3a5e84b591ee8d208b28765ff0feacc2b2995
Joseph Semlak <semlak@gmail.com>
2018-03-25 20:10:37 -0500

    updated story api route so that the get request can be passed an optional parameter ordering set to either 'ASC' or 'DESC'

--- routes/story-api-routes.js
+++ routes/story-api-routes.js
@@ -1,9 +1,16 @@
 var db = require("../models");
+const defaultOrdering = "ASC";
+
 
 module.exports = function(app) {
   app.get("/api/stories", function(req, res) {
+    // console.log("req.query", req.query)
+    var storiesOrdering = req.query && req.query.ordering && req.query.ordering.toUpperCase();
+    // verify that storiesOrdering is either "ASC" or "DESC", otherwise, reset to default ordering
+    storiesOrdering = storiesOrdering === "ASC" || storiesOrdering === "DESC" ? storiesOrdering : defaultOrdering;
     db.Story.findAll({
-      include: [db.Turn, db.Player]
+      include: [db.Turn, db.Player],
+      order: [["updatedAt", storiesOrdering]]
     }).then(function(stories) {
       res.json(stories)
     });
